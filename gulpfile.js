9059e765799dde961fcf0c3ab9b1a3a784e45765
Claudio Bredfeldt <claudio@cbmono.de>
2016-10-20 01:22:04 +0200

    Fix conflicts

--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,13 @@ const gulp = require('gulp');
 //
 // Variables
 //
-<<<<<<< 22b7df431f50caf2ee584ef803e092a94fd67e8a
 const gulpTasksSrc = './tasks/**/gulp.*.js';
-=======
-var gulpTasksSrc = './libs/tasks/**/gulp.*.js'
->>>>>>> Split hapijs plguin's definition (#57)
 
 //
 // Load Gulp Tasks
 //
 glob.sync(gulpTasksSrc)
-<<<<<<< 22b7df431f50caf2ee584ef803e092a94fd67e8a
   .forEach((file) => require(path.resolve(file)));
-=======
-  .forEach((file) => require(path.resolve(file)))
->>>>>>> Split hapijs plguin's definition (#57)
 
 //
 // Default (gulp) task
@@ -33,7 +25,8 @@ glob.sync(gulpTasksSrc)
 //  * Lint Javascript files
 //  * Run local node server
 //  * Resatart on file changes
+//
 gulp.task('default', [
   'lint',
-  'nodemon',
+  'nodemon'
 ]);
